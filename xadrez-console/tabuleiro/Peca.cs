88c5704ea4c06e4bd447876edb0407b27496cbee
Felipe Freitas <ffrfelipefreitas@gmail.com>
2022-02-13 22:12:37 -0300

    Criando classe Peca

---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QtdMovimentos { get; protected set; }
+        public Tabuleiro Tab { get; set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            this.Posicao = posicao;
+            this.Cor =cor;
+            this.Tab = tab;
+            this.QtdMovimentos = 0;
+        }
+    }
+}
