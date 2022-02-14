30de95a13d736bef569995c16474bb2a6ed51acc
Felipe Freitas <felipefreitasffr@gmail.com>
2022-02-14 13:02:33 -0300

    Criada classe tabuleiro

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -8,5 +8,16 @@ namespace xadrez_console.tabuleiro
 {
     class Tabuleiro
     {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        public Peca[,] Pecas;
+
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            Pecas = new Peca[linhas, colunas];
+        }
     }
 }
